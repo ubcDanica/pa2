d33ca8339deb8d7d3b82bb0875ec7d9cb0a0a77e
Chuxin Xiao <g1x1b@anvil.ugrad.cs.ubc.ca>
2019-02-21 23:57:05 -0800

    stripe

--- stripeColorPicker.cpp
+++ stripeColorPicker.cpp
@@ -2,10 +2,21 @@
 
 stripeColorPicker::stripeColorPicker(HSLAPixel fillColor, int stripeSpacing)
 {
+	color = fillColor;
+	spacing = stripeSpacing;
 /* your code here! */
 }
 
 HSLAPixel stripeColorPicker::operator()(int x, int y)
 {
+	if(x/spacing==0 || y/spacing==0)
+		return color;
+	else{
+		HSLAPixel white;
+		white.h = 0;
+		white.s = 0;
+		white.l = 1;
+		return white;
+	}
 /* your code here! */
 }
