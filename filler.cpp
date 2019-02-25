7c77fbc9d034dec46667adcc3cb8fd5659e142eb
fishmankevinyu <kaikailongwang3@gmail.com>
2019-02-24 17:35:20 -0800

    jiy

--- filler.cpp
+++ filler.cpp
@@ -109,7 +109,7 @@ animation filler::fill(PNG& img, int x, int y, colorPicker& fillColor,
 
 
         HSLAPixel *pixel = img.getPixel((unsigned)x0,(unsigned)y0);
-        *pixel = fillColor(x,y);
+        *pixel = fillColor.operator()(x,y);
         fill++;
 
         std::map<std::pair<int,int>,bool>::iterator marked = mark.find(pair<int,int>(x0,y0));
@@ -124,12 +124,14 @@ animation filler::fill(PNG& img, int x, int y, colorPicker& fillColor,
     }
     anim.addFrame(img);
     //anim.write(anim);
+
+    return anim;
     
 }
     bool filler::canAdd(PNG& img, int x,int y,std::map<std::pair<int,int>, bool> mark, double tolerance, HSLAPixel *center){
         if(x<=(int)img.width() && x>0 && y<=(int)img.height() && y>0){
             HSLAPixel *pixel = img.getPixel(x,y);
-            if(mark.at(pair<int,int>(x,y)) == false && !((*center+tolerance<*pixel) || (*pixel<*center-tolerance))){
+            if(mark.at(pair<int,int>(x,y)) == false && (center->dist(*pixel)<= tolerance)){
                 return true;
             }
             else
